420bcdd9ee0e66b5b158083c098fecc972863bdd
AmjadIG <amjadmenouer@gmail.com>
2020-11-01 12:54:14 +0100

    test

--- covidalert/src/App.js
+++ covidalert/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
-import AlertsCenter from './components/Pages/AlertsCenter/AlertsCenter.js';
+//import AlertsCenter from './components/Pages/AlertsCenter/AlertsCenter.js';
 import Login from './components/Pages/Login/Login';
-import Footer from './components/Template/Footer/Footer';
+//import Footer from './components/Template/Footer/Footer';
 import Header from './components/Template/Header/Header.js'
 
 function App() {
