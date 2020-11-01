8a25496d8aa3dfb3f84a320b46b7b8283a5a97be
Polyrayan <32355939+Polyrayan@users.noreply.github.com>
2020-11-01 12:47:31 +0100

    add location view

--- covidalert/src/App.js
+++ covidalert/src/App.js
@@ -3,12 +3,15 @@ import AlertsCenter from './components/Pages/AlertsCenter/AlertsCenter.js';
 import Login from './components/Pages/Login/Login';
 import Footer from './components/Template/Footer/Footer';
 import Header from './components/Template/Header/Header.js'
+import Location from "./components/Location/Location";
+import React from 'react';
 
 function App() {
   return (
     <div className="App">
-      <Header></Header>
-      <Login></Login>
+      <Header/>
+      <Login/>
+      <Location/>
     </div>
     
   );
