70f1e6293cf056be84371db45e227354bca949d8
Polyrayan <32355939+Polyrayan@users.noreply.github.com>
2020-11-09 14:15:17 +0100

    fix state front

--- covidalert/src/components/State/State.js
+++ covidalert/src/components/State/State.js
@@ -10,37 +10,41 @@ class State extends React.Component {
             contact : false,
             health: "Vous êtes en bonne santé!"
         }
-        this.changeHealthState = this.changeHealthState.bind(this);
-        this.changeCondition = this.changeCondition.bind(this);
+        this.beSane = this.beSane.bind(this);
+        this.beSick = this.beSick.bind(this);
+        this.beContact = this.beContact.bind(this);
+        this.loadData = this.loadData.bind(this);
     }
 
-    changeCondition(){
-        if(this.state.sane && this.state.contact){
-            this.setState({health:"Vous êtes cas contact"});
-        }else if(this.state.sane){
-            this.setState({health:"Vous êtes en bonne santé!"});
-        }
-        this.setState({
-            contact : !this.state.contact
-        })
+    componentDidMount() {
+        this.loadData()
     }
 
-    changeHealthState(){
-        if(!this.state.sane){
-            this.setState({health:"Vous avez le covid"});
-        }else if(this.state.sane && this.state.contact){
-            this.setState({health:"Vous êtes cas contact"});
-        }else if(this.state.sane){
-            this.setState({health:"Vous êtes en bonne santé!"});
-        }
-        this.setState({
-            sane : !this.state.sane
-        })
+    // todo load data from db
+    loadData(){
+       console.log("load data")
     }
 
+    beSane(){
+        this.setState({sane : true});
+        this.setState({contact : false});
+        this.setState({health : "Vous êtes en bonne santé!"});
+    }
+
+    beContact() {
+        this.setState({sane : true});
+        this.setState({contact : true});
+        this.setState({health: "Vous êtes cas contact"});
+    }
+
+
+    beSick(){
+        this.setState({sane : false});
+        this.setState({health : "Vous avez le covid"});
+    }
     render(){
-        let button1 = this.state.sane ? <button className="button covid" onClick={this.changeHealthState}>J'ai le covid</button> : <button className="button sane" onClick={this.changeHealthState}>Je n'ai plus le covid</button>
-        let button2 = this.state.contact ? <button className="button sane" onClick={this.changeCondition}>Je ne suis plus cas contact</button> : <button className="button contact" onClick={this.changeCondition}>Je suis cas contact</button>
+        let button1 = this.state.sane ? <button className="button covid" onClick={this.beSick}>J'ai le covid</button> : <button className="button sane" onClick={this.beSane}>Je n'ai plus le covid</button>
+        let button2 = this.state.contact ? <button className="button sane" onClick={this.beSane}>Je ne suis plus cas contact</button> : <button className="button contact" onClick={this.beContact}>Je suis cas contact</button>
         return(
             <div className="state-component">
                 <h2>Votre état de Santé</h2>
