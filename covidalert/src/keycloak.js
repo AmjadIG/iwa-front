23f26f19e5ccf32f81184a14f72c5e212e58f958
Polyrayan <32355939+Polyrayan@users.noreply.github.com>
2020-11-12 18:01:23 +0100

    Revert "Revert "keycloak login page but new issue => redirection?""
    This reverts commit 31a666b3d5ed2c1794055a08d2c10999bcd837a2.

--- covidalert/src/keycloak.js
+++ covidalert/src/keycloak.js
@@ -2,7 +2,8 @@ import Keycloak from 'keycloak-js'
 const keycloakConfig = {
     url: 'http://localhost:8081/auth/',
     realm: 'iwa',
-    clientId: 'iwa-api'
+    clientId: 'iwa-api',
+    onLoad: 'login-required'
 }
 const keycloak = new Keycloak(keycloakConfig);
 export default keycloak;
